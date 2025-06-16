79c9d880b4e17d14fb658fdf5a24ca67dcdfb396
Carlos Ignacio Azócar Huerta <carlos.iah17@gmail.com>
2025-06-15 21:03:41 -0400

    Experience update
    Added new job to the list

--- src/static/ts/scripts.ts
+++ src/static/ts/scripts.ts
@@ -1,235 +1,255 @@
 const body = document.body;
 
-const btnTheme = document.querySelector('.fa-moon') as HTMLElement;
-const btnHamburger = document.querySelector('.fa-bars') as HTMLElement;
+const btnTheme = document.querySelector(".fa-moon") as HTMLElement;
+const btnHamburger = document.querySelector(".fa-bars") as HTMLElement;
 
 const addThemeClass = (bodyClass: string, btnClass: string) => {
   body.classList.add(bodyClass);
   btnTheme.classList.add(btnClass);
 };
 
-const getBodyTheme = localStorage.getItem('portfolio-theme');
-const getBtnTheme = localStorage.getItem('portfolio-btn-theme');
+const getBodyTheme = localStorage.getItem("portfolio-theme");
+const getBtnTheme = localStorage.getItem("portfolio-btn-theme");
 
 addThemeClass(getBodyTheme!, getBtnTheme!);
 
-const isDark = () => body.classList.contains('dark');
+const isDark = () => body.classList.contains("dark");
 
 const setTheme = (bodyClass: string, btnClass: string) => {
-  body.classList.remove(localStorage.getItem('portfolio-theme')!);
-  btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme')!);
+  body.classList.remove(localStorage.getItem("portfolio-theme")!);
+  btnTheme.classList.remove(localStorage.getItem("portfolio-btn-theme")!);
 
   addThemeClass(bodyClass, btnClass);
 
-  localStorage.setItem('portfolio-theme', bodyClass);
-  localStorage.setItem('portfolio-btn-theme', btnClass);
+  localStorage.setItem("portfolio-theme", bodyClass);
+  localStorage.setItem("portfolio-btn-theme", btnClass);
 };
 
 const toggleTheme = () =>
-  isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun');
+  isDark() ? setTheme("light", "fa-moon") : setTheme("dark", "fa-sun");
 
-btnTheme.addEventListener('click', toggleTheme);
+btnTheme.addEventListener("click", toggleTheme);
 
 const displayList = () => {
-  const navUl = document.querySelector('.nav__list') as HTMLElement;
+  const navUl = document.querySelector(".nav__list") as HTMLElement;
 
-  if (btnHamburger.classList.contains('fa-bars')) {
-    btnHamburger.classList.remove('fa-bars');
-    btnHamburger.classList.add('fa-times');
-    navUl.classList.add('display-nav-list');
+  if (btnHamburger.classList.contains("fa-bars")) {
+    btnHamburger.classList.remove("fa-bars");
+    btnHamburger.classList.add("fa-times");
+    navUl.classList.add("display-nav-list");
   } else {
-    btnHamburger.classList.remove('fa-times');
-    btnHamburger.classList.add('fa-bars');
-    navUl.classList.remove('display-nav-list');
+    btnHamburger.classList.remove("fa-times");
+    btnHamburger.classList.add("fa-bars");
+    navUl.classList.remove("display-nav-list");
   }
 };
 
-btnHamburger.addEventListener('click', displayList);
+btnHamburger.addEventListener("click", displayList);
 
 const scrollUp = () => {
-  const btnScrollTop = document.querySelector('.scroll-top') as HTMLElement;
+  const btnScrollTop = document.querySelector(".scroll-top") as HTMLElement;
 
   if (body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    btnScrollTop.style.display = 'block';
+    btnScrollTop.style.display = "block";
   } else {
-    btnScrollTop.style.display = 'none';
+    btnScrollTop.style.display = "none";
   }
 };
 
-document.addEventListener('scroll', scrollUp);
+document.addEventListener("scroll", scrollUp);
 
 // Language Switch
 
 const translations: Record<string, Record<string, string>> = {
-  'greetings': {
-    en: 'Hi, I am ',
-    es: 'Hola, soy ',
+  greetings: {
+    en: "Hi, I am ",
+    es: "Hola, soy ",
   },
-  'myname': {
-    en: 'Carlos Azócar',
-    es: 'Carlos Azócar',
+  myname: {
+    en: "Carlos Azócar",
+    es: "Carlos Azócar",
   },
-  'specialty': {
-    en: 'Full-Stack Developer.',
-    es: 'Desarrollador Full-Stack.',
+  specialty: {
+    en: "Full-Stack Developer.",
+    es: "Desarrollador Full-Stack.",
   },
-  'aboutme': {
-    en: 'Independent and proactive. I develop with dedication, breathing life into each line of code with care, commitment, and enthusiasm.',
-    es: 'Autónomo y proactivo. Desarrollo con dedicación, dando vida a cada línea de código con cariño, compromiso y entusiasmo.',
+  aboutme: {
+    en: "Independent and proactive. I develop with dedication, breathing life into each line of code with care, commitment, and enthusiasm.",
+    es: "Autónomo y proactivo. Desarrollo con dedicación, dando vida a cada línea de código con cariño, compromiso y entusiasmo.",
   },
-  'experience': {
-    en: 'Experience',
-    es: 'Experiencia',
+  experience: {
+    en: "Experience",
+    es: "Experiencia",
   },
-  'expertise': {
-    en: 'Expertise',
-    es: 'Conocimientos',
+  expertise: {
+    en: "Expertise",
+    es: "Conocimientos",
   },
-  'projects': {
-    en: 'Projects',
-    es: 'Proyectos',
+  projects: {
+    en: "Projects",
+    es: "Proyectos",
   },
-  'contact': {
-    en: 'Contact',
-    es: 'Contacto',
+  contact: {
+    en: "Contact",
+    es: "Contacto",
   },
-  'emailme': {
-    en: 'Email me',
-    es: 'Enviar email',
+  emailme: {
+    en: "Email me",
+    es: "Enviar email",
   },
-  'project1': {
-    en: 'YouTube Media Downloader',
-    es: 'YouTube Media Downloader',
+  project1: {
+    en: "YouTube Media Downloader",
+    es: "YouTube Media Downloader",
   },
-  'project1desc': {
-    en: 'YT media downloader created with Next.js and ytdl-core.',
-    es: 'Descargador de contenido de YT creado con Next.js y ytdl-core.',
+  project1desc: {
+    en: "YT media downloader created with Next.js and ytdl-core.",
+    es: "Descargador de contenido de YT creado con Next.js y ytdl-core.",
   },
-  'project2': {
-    en: 'Tetris',
-    es: 'Tetris',
+  project2: {
+    en: "Tetris",
+    es: "Tetris",
   },
-  'project2desc': {
-    en: 'Classic Tetris game created with Canvas, vanilla JavaScript and CSS.',
-    es: 'Clásico juego de Tetris desarrollado con Canvas, JavaScript nativo y CSS.',
+  project2desc: {
+    en: "Classic Tetris game created with Canvas, vanilla JavaScript and CSS.",
+    es: "Clásico juego de Tetris desarrollado con Canvas, JavaScript nativo y CSS.",
   },
-  'project3': {
-    en: 'URL Shortener',
-    es: 'Acortador de URL',
+  project3: {
+    en: "URL Shortener",
+    es: "Acortador de URL",
   },
-  'project4': {
-    en: 'Echo AI assistant',
-    es: 'Asistente Echo IA',
+  project4: {
+    en: "Echo AI assistant",
+    es: "Asistente Echo IA",
   },
-  'project5': {
-    en: 'Screen Recorder',
-    es: 'Grabador de Pantalla',
+  project5: {
+    en: "Screen Recorder",
+    es: "Grabador de Pantalla",
   },
-  'project3desc': {
-    en: 'URL Shortener that uses my own API created with Express and MongoDB.',
-    es: 'Acortador de URL que utiliza mi propia API creada con Express y MongoDB',
+  project3desc: {
+    en: "URL Shortener that uses my own API created with Express and MongoDB.",
+    es: "Acortador de URL que utiliza mi propia API creada con Express y MongoDB",
   },
-  'project4desc': {
-    en: 'An AI voice assistant created with Next.js, Groq and Elevenlabs.',
-    es: 'Asistente IA con voz creado con Next.js, Groq y Elevenlabs.',
+  project4desc: {
+    en: "An AI voice assistant created with Next.js, Groq and Elevenlabs.",
+    es: "Asistente IA con voz creado con Next.js, Groq y Elevenlabs.",
   },
-  'project5desc': {
-    en: 'A screen recorder developed with JS, Astro and Tailwind CSS.',
-    es: 'Grabador de pantalla desarrollado con JS, Astro y Tailwind CSS.',
+  project5desc: {
+    en: "A screen recorder developed with JS, Astro and Tailwind CSS.",
+    es: "Grabador de pantalla desarrollado con JS, Astro y Tailwind CSS.",
   },
-  'cv': {
-    en: 'Download CV',
-    es: 'Descargar CV',
+  cv: {
+    en: "Download CV",
+    es: "Descargar CV",
   },
-  'footer': {
-    en: 'Made with love &lt;3',
-    es: 'Hecho con amor &lt;3',
+  footer: {
+    en: "Made with love &lt;3",
+    es: "Hecho con amor &lt;3",
   },
-  'formemail': {
-    en: 'Your email:',
-    es: 'Tu email:',
+  formemail: {
+    en: "Your email:",
+    es: "Tu email:",
   },
-  'formmessage': {
-    en: 'Your message:',
-    es: 'Tu mensaje:',
+  formmessage: {
+    en: "Your message:",
+    es: "Tu mensaje:",
   },
-  'formbutton': {
-    en: 'Send',
-    es: 'Enviar',
+  formbutton: {
+    en: "Send",
+    es: "Enviar",
   },
-  'expduoctitle': {
-    en: 'Professional Internship: Developer',
-    es: 'Práctica Profesional: Desarrollador',
+  expmunicipalidadtitle: {
+    en: "Software Developer",
+    es: "Desarrollador de Software",
   },
-  'expduocdate': {
-    en: 'September 2023 - December 2023',
-    es: 'Septiembre 2023 - Diciembre 2023',
+  expmunicipalidaddate: {
+    en: "December 2024 - Present",
+    es: "Diciembre 2024 - Actualidad",
   },
-  'expduocabout': {
-    en: 'Development of a web management system for documents and resources.',
-    es: 'Desarrollo de un sistema de gestión web para documentos y recursos.',
+  expmunicipalidadabout: {
+    en: "Software Development, legacy software migration, and server management",
+    es: "Desarrollo de software, migración de software legacy y gestión de servidores",
   },
-  'expduocdesc': {
-    en: 'The implementation of the web system brought the following benefits to the company: efficient task automation, remote access to information, centralization and organization of resources, data security, and improvement in data-driven decision-making.',
-    es: 'La implementación del sistema web aportó los siguientes beneficios a la empresa: automatización eficiente de tareas, acceso remoto a la información, centralización y organización de recursos, seguridad de datos y mejora en la toma de decisiones basada en datos.',
+  expmunicipalidaddesc: {
+    en: "I’ve developed multiple websites according to the company’s needs, migrated legacy software to more modern technologies, managed application servers, databases, and overseen various projects.",
+    es: "He desarrollado múltiples sitios web según las necesidades de la empresa, migrado software legacy a tecnologías más modernas, gestionado servidores de aplicaciones, bases de datos y supervisado diversos proyectos.",
   },
-  'exptvtitle': {
-    en: 'Work Placement: Developer',
-    es: 'Práctica Profesional: Desarrollador',
+
+  expduoctitle: {
+    en: "Professional Internship: Developer",
+    es: "Práctica Profesional: Desarrollador",
   },
-  'exptvdate': {
-    en: 'February 2023 - March 2023',
-    es: 'Febrero 2023 - Marzo 2023',
+  expduocdate: {
+    en: "September 2023 - December 2023",
+    es: "Septiembre 2023 - Diciembre 2023",
   },
-  'exptvabout': {
-    en: 'Maintenance, error correction, and implementation of new functionalities for the company\'s website.',
-    es: 'Mantenimiento, corrección de errores e implementación de nuevas funcionalidades para el sitio web de la empresa.',
+  expduocabout: {
+    en: "Development of a web management system for documents and resources.",
+    es: "Desarrollo de un sistema de gestión web para documentos y recursos.",
   },
-  'exptvdesc': {
-    en: 'Website maintenance, error fixes, and enhancements yielded notable improvements for the company, ensuring a secure, efficient, and feature-rich online environment.',
-    es: 'El mantenimiento del sitio web, las correcciones de errores y las mejoras proporcionaron mejoras notables para la empresa, garantizando un entorno en línea seguro, eficiente y rico en funciones.',
+  expduocdesc: {
+    en: "The implementation of the web system brought the following benefits to the company: efficient task automation, remote access to information, centralization and organization of resources, data security, and improvement in data-driven decision-making.",
+    es: "La implementación del sistema web aportó los siguientes beneficios a la empresa: automatización eficiente de tareas, acceso remoto a la información, centralización y organización de recursos, seguridad de datos y mejora en la toma de decisiones basada en datos.",
   },
-  'toastSuccess': {
-    en: 'Message Sent',
-    es: 'Mensaje enviado',
+  exptvtitle: {
+    en: "Work Placement: Developer",
+    es: "Práctica Profesional: Desarrollador",
   },
-  'toastError': {
-    en: 'Error',
-    es: 'Error',
+  exptvdate: {
+    en: "February 2023 - March 2023",
+    es: "Febrero 2023 - Marzo 2023",
   },
-  'extra': {
-    en: 'Extra',
-    es: 'Extra',
+  exptvabout: {
+    en: "Maintenance, error correction, and implementation of new functionalities for the company's website.",
+    es: "Mantenimiento, corrección de errores e implementación de nuevas funcionalidades para el sitio web de la empresa.",
   },
-  'extra1': {
-    en: 'Full Stack JS Course',
-    es: 'Curso Full Stack de JS',
+  exptvdesc: {
+    en: "Website maintenance, error fixes, and enhancements yielded notable improvements for the company, ensuring a secure, efficient, and feature-rich online environment.",
+    es: "El mantenimiento del sitio web, las correcciones de errores y las mejoras proporcionaron mejoras notables para la empresa, garantizando un entorno en línea seguro, eficiente y rico en funciones.",
   },
-  'extra1desc': {
-    en: 'I am currently enrolled in the certified Full Stack Open online course, which is based on JavaScript and offered by the University of Helsinki.',
-    es: 'Actualmente estoy inscrito en el curso en línea certificado Full Stack Open, el cual se basa en JavaScript y es ofrecido por la Universidad de Helsinki.',
+  toastSuccess: {
+    en: "Message Sent",
+    es: "Mensaje enviado",
   },
-  'extra2': {
-    en: 'Real-Time Chat Course',
-    es: 'Curso de Chat en Tiempo Real',
+  toastError: {
+    en: "Error",
+    es: "Error",
   },
-  'extra2desc': {
-    en: 'I completed a Full Stack project following a real-time chat course akin to Messenger, which enabled me to delve into Front-End, Back-End, Authentication with other platforms, as well as real-time features such as user status and messaging.',
-    es: 'Completé un proyecto Full Stack siguiendo un curso de chat en tiempo real similar a Messenger, lo que me permitió adentrarme en el Front-End, Back-End, autenticación con otras plataformas y características en tiempo real como el estado del usuario y la mensajería.',
+  extra: {
+    en: "Extra",
+    es: "Extra",
+  },
+  extra1: {
+    en: "Full Stack JS Course",
+    es: "Curso Full Stack de JS",
+  },
+  extra1desc: {
+    en: "I am currently enrolled in the certified Full Stack Open online course, which is based on JavaScript and offered by the University of Helsinki.",
+    es: "Actualmente estoy inscrito en el curso en línea certificado Full Stack Open, el cual se basa en JavaScript y es ofrecido por la Universidad de Helsinki.",
+  },
+  extra2: {
+    en: "Real-Time Chat Course",
+    es: "Curso de Chat en Tiempo Real",
+  },
+  extra2desc: {
+    en: "I completed a Full Stack project following a real-time chat course akin to Messenger, which enabled me to delve into Front-End, Back-End, Authentication with other platforms, as well as real-time features such as user status and messaging.",
+    es: "Completé un proyecto Full Stack siguiendo un curso de chat en tiempo real similar a Messenger, lo que me permitió adentrarme en el Front-End, Back-End, autenticación con otras plataformas y características en tiempo real como el estado del usuario y la mensajería.",
   },
 };
 
 export default translations;
 
-let currentLanguage: 'en' | 'es' = 'en';
+let currentLanguage: "en" | "es" = "en";
 
-const btnLanguage = document.getElementById('btn-language');
+const btnLanguage = document.getElementById("btn-language");
 
 const translateElement = (element: HTMLElement) => {
-  const key = element.getAttribute('data-translate-key');
+  const key = element.getAttribute("data-translate-key");
   if (key && translations[key]) {
-    if (element.tagName.toLowerCase() === 'h1' && element.querySelector('.about__name')) {
-      element.innerHTML = `${translations[key][currentLanguage]}<span class="about__name">${translations['myname'][currentLanguage]}</span>.`;
+    if (
+      element.tagName.toLowerCase() === "h1" &&
+      element.querySelector(".about__name")
+    ) {
+      element.innerHTML = `${translations[key][currentLanguage]}<span class="about__name">${translations["myname"][currentLanguage]}</span>.`;
     } else {
       element.innerHTML = translations[key][currentLanguage];
     }
@@ -237,9 +257,9 @@ const translateElement = (element: HTMLElement) => {
 };
 
 const toggleLanguage = () => {
-  currentLanguage = currentLanguage === 'en' ? 'es' : 'en';
+  currentLanguage = currentLanguage === "en" ? "es" : "en";
 
-  const elementsToTranslate = document.querySelectorAll('.translate');
+  const elementsToTranslate = document.querySelectorAll(".translate");
 
   elementsToTranslate.forEach((element) => {
     translateElement(element as HTMLElement);
@@ -247,19 +267,23 @@ const toggleLanguage = () => {
 };
 
 if (btnLanguage) {
-  btnLanguage.addEventListener('click', toggleLanguage);
+  btnLanguage.addEventListener("click", toggleLanguage);
 }
-
 
 // cv download
 
-const downloadCvButton = document.querySelector('.btn--download-cv') as HTMLElement;
+const downloadCvButton = document.querySelector(
+  ".btn--download-cv"
+) as HTMLElement;
 
 const downloadCv = () => {
-  const cvFileName = currentLanguage === 'en' ? 'Resume_Carlos_Azocar.pdf' : 'CV_Carlos_Azocar.pdf';
+  const cvFileName =
+    currentLanguage === "en"
+      ? "Resume_Carlos_Azocar.pdf"
+      : "CV_Carlos_Azocar.pdf";
   const cvFilePath = `/${cvFileName}`;
 
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = cvFilePath;
   link.download = cvFileName;
 
@@ -269,5 +293,5 @@ const downloadCv = () => {
 };
 
 if (downloadCvButton) {
-  downloadCvButton.addEventListener('click', downloadCv);
+  downloadCvButton.addEventListener("click", downloadCv);
 }
